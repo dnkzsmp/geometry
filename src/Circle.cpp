c6654e01df672e804aaba0388961d27d35ee8b34
Danil Kozlov <danilkozlov1003@gmail.com>
2020-04-12 19:48:41 +0700

    Changed if-statement

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,20 +1,18 @@
-#include <stdio.h>
 #define p 3.14
 
 float Perimetr(float r){
   if (r <= 0) {
-    printf("Неверный радиус");
-    return 0;
+    return 1;
   }
   float P = p * r * 2;
   return P;
 }
 
 float Ploshad(float r) {
+  float S;
   if (r <= 0) {
-    printf("Неверный радиус");
-    return 0;
-  }
-  float S = p * r * r;
+    return 1;
+  } else
+    S = p * r * r;
   return S;
 }
