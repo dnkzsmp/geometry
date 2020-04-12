e7ea9599942e760bb4dfa98ea314059b95500663
Danil Kozlov <danilkozlov1003@gmail.com>
2020-04-12 19:49:15 +0700

    Changed checking of correct input data

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "Figure.h"
 #include <stdio.h>
 
-
 int main() {
   float r = 0;
   float P;
@@ -18,7 +17,15 @@ int main() {
     printf("Радиус: \n");
     scanf("%f", &r);
     S = Ploshad(r);
+    if (S == 1) {
+      printf("Неверный радиус!");
+      return 0;
+    }
     P = Perimetr(r);
+    if (P == 1) {
+      printf("Неверный радиус!");
+      return 0;
+    }
   } else {
     printf("Ошибка ввода фигуры! Доступные фигуры: circle");
     return 0;
