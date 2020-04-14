b392ed2d507314dd2d0232fd727625b75be04780
Danil Kozlov <danilkozlov1003@gmail.com>
2020-04-14 23:27:03 +0700

    Fix code's format

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,18 +1,20 @@
 #define p 3.14
 
-float Perimetr(float r) {
-  if (r <= 0) {
-    return 1;
-  }
-  float P = p * r * 2;
-  return P;
+float Perimetr(float r)
+{
+    if (r <= 0) {
+        return 1;
+    }
+    float P = p * r * 2;
+    return P;
 }
 
-float Ploshad(float r) {
-  float S;
-  if (r <= 0) {
-    return 1;
-  } else
-    S = p * r * r;
-  return S;
+float Ploshad(float r)
+{
+    float S;
+    if (r <= 0) {
+        return 1;
+    } else
+        S = p * r * r;
+    return S;
 }
