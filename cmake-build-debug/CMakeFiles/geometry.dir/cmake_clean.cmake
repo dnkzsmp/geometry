d8a168dada51dd225df959d8f20ca046a8006a53
Danil Kozlov <danilkozlov1003@gmail.com>
2020-04-25 19:57:17 +0700

    Init of CLion

---
+++ cmake-build-debug/CMakeFiles/geometry.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geometry.dir/src/main.c.o"
+  "CMakeFiles/geometry.dir/src/Circle.c.o"
+  "CMakeFiles/geometry.dir/src/Figure.c.o"
+  "CMakeFiles/geometry.dir/src/Intersection.c.o"
+  "CMakeFiles/geometry.dir/test/test_intersection.c.o"
+  "geometry.pdb"
+  "geometry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/geometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
